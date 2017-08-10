857f05eb2a2126475c85aaab07a72e0d2de6c58b
Yolanda Robla <info@ysoft.biz>
2017-08-10 09:25:16 -0400

    Update script to cover new data format for mirrors

--- scripts/create_repo.sh
+++ scripts/create_repo.sh
@@ -1,5 +1,6 @@
 REPO_FOLDER=$1
 REPO_NAME=$2
+REPO_ITEMS=$3
 PUBLIC_ADDRESS=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 PUBLIC_ADDRESS=($PUBLIC_ADDRESS)
 REPOPATH="/var/ftp/pub/"
@@ -9,11 +10,21 @@ mkdir -p $REPOPATH
 rm $REPOFILE 2> /dev/null
 touch $REPOFILE
 
-for DIR in `find ${REPOPATH}${REPO_FOLDER}/* -maxdepth 1 -mindepth 1 -type d`; do
-    echo -e "[`basename $DIR`]" >> $REPOFILE
-    echo -e "name=`basename $DIR`" >> $REPOFILE
-    echo -e "baseurl=ftp://${PUBLIC_ADDRESS}/pub/${REPO_FOLDER}/`basename $DIR`/" >> $REPOFILE
-    echo -e "enabled=1" >> $REPOFILE
+echo "${REPOPATH}${REPO_FOLDER}"
+
+for DIR in `find ${REPOPATH}general_mirror/ -maxdepth 1 -mindepth 1 -type d`; do
+    REPO_ITEM=$(basename $DIR)
+    echo -e "[${REPO_ITEM}]" >> $REPOFILE
+    echo -e "name=${REPO_ITEM}" >> $REPOFILE
+    echo -e "baseurl=ftp://${PUBLIC_ADDRESS}/pub/${REPO_FOLDER}/${REPO_ITEM}/" >> $REPOFILE
+
+    # only enable if repo is in the list
+    if [[ "${REPO_ITEMS}" =~ "${REPO_ITEM}" ]]; then
+        echo -e "enabled=1" >> $REPOFILE
+    else
+        echo -e "enabled=0" >> $REPOFILE
+    fi
+
     echo -e "gpgcheck=1" >> $REPOFILE
     echo -e "gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release" >> $REPOFILE
     echo -e "\n" >> $REPOFILE
