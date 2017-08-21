f8c3a21e2f3592d5d306368cbf584c2f6a3db230
Yolanda Robla <yroblamo@redhat.com>
2017-08-21 13:43:38 +0200

    Add possibility to configure ip for mirror
    Allow to pass the var to define the external ip of the
    mirror. If not, extract the first ipv4 from the server.

--- scripts/create_repo.sh
+++ scripts/create_repo.sh
@@ -1,8 +1,17 @@
 REPO_FOLDER=$1
 REPO_NAME=$2
 REPO_ITEMS=$3
+MIRROR_IP_ADDRESS=$4
 PUBLIC_ADDRESS=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 PUBLIC_ADDRESS=($PUBLIC_ADDRESS)
+
+if [ -z "$MIRROR_IP_ADDRESS" ]; then
+  PUBLIC_ADDRESS=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+  PUBLIC_ADDRESS=($PUBLIC_ADDRESS)
+else
+  PUBLIC_ADDRESS=$MIRROR_IP_ADDRESS
+fi
+
 REPOPATH="/var/www/mirrors/"
 REPOFILE="${REPOPATH}/${REPO_FOLDER}/${REPO_NAME}"
 
